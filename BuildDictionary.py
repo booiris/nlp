a55fc4a3a218d623b045f7b9e4b06d80ee4a2cb4
booiris <954520770@qq.com>
2020-10-31 16:15:59 +0800

    update

--- BuildDictionary.py
+++ BuildDictionary.py
@@ -1,5 +1,5 @@
 import pickle
-
+import string
 
 f = open("varname.txt", "rb")
 varname = pickle.load(f)
@@ -8,6 +8,26 @@ transmat = varname["transmat"]
 word_list = varname["word_list"]
 speech_list = varname["speech_list"]
 
+punc = string.punctuation
+
+cnt1 = 0
+cnt2 = 0
 for word in word_list:
     if len(word_list[word]) == 1:
-        print(word)
+        flag = True
+        for i in word:
+            if i in punc:
+                flag = False
+                break
+        if flag:
+            cnt1 += 1
+
+    else:
+        temp = sorted(word_list[word].items(), key=lambda item: item[1], reverse=True)
+        word_list[word] = dict(temp)
+        cnt2 += 1
+
+print("非兼类词数量:",cnt1)
+print("兼类词数量:",cnt2)
+print("科学:",word_list["科学"])
+print("方便:",word_list["方便"])
