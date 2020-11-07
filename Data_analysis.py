3b0b57f9aee584b6ca8865ee2a936a076f00a82b
booiris <954520770@qq.com>
2020-11-07 19:37:54 +0800

    update

--- Data_analysis.py
+++ Data_analysis.py
@@ -1,8 +1,15 @@
 import arpa
-import pickle
 
 
-models = arpa.loadf("train_lm.txt",encoding="utf-8")
-
+models = arpa.loadf("train_lm.txt",encoding="gbk")
+print(models)
+print(type(models))
+print(len(models))
 lm = models[0]
-print(lm.p("大家 好"))
+print(type(lm))
+print(lm.counts())
+print(lm.log_p("今天 真 不错"))
+print(lm.log_s("都 是 为了 完善 社会主义 制度"))
+print(lm.log_s("为了 是 都 制度 社会主义 完善"))
+
+
